504199fc0e552b56ccc687a5df3d7fd1b838d8a4
SM_SUMON <42109376+sumonmd@users.noreply.github.com>
2019-05-27 23:06:45 +0600

    Add files via upload

---
+++ 1st.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+   int number;
+   scanf("%d",&number);
+   if(number<0){
+        printf("negative");
+   }
+   else{
+    printf("positive");
+   }
+    return  0;
+}
